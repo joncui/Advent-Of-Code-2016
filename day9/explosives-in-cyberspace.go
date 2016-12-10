2962b77b55917703508d49ef3a95acc5e82ac690
Jonathan Cui <jonathan.cui@hulu.com>
2016-12-09 19:32:35 -0800

    Complete Day9

---
+++ day9/explosives-in-cyberspace.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+    "fmt"
+    "io/ioutil"
+    "strconv"
+    "strings"
+)
+
+func check(e error) {
+    if e != nil {
+        panic(e)
+    }
+}
+
+func main() {
+    data, err := ioutil.ReadFile("/Users/jonathan.cui/go/src/github.com/joncui/Advent-Of-Code-2016/day9/data")
+    check(err)
+
+    dataString := strings.TrimSpace(string(data))
+
+    count1 := partOne(dataString)
+    count2 := partTwo(dataString)
+
+    fmt.Printf("Decompressed length pt 1: %d\n", count1)
+    fmt.Printf("Decompressed length pt 2: %d\n", count2)
+}
+
+func partOne(data string) (count int) {
+    markerStart := strings.Index(data, "(")
+    markerEnd := strings.Index(data, ")")
+    if (markerStart == -1) || (markerEnd == -1) {
+        count += len(data)
+    } else {
+        if markerStart != 0 {
+            count += markerStart
+        }
+
+        x, y := parseMarker(data[markerStart + 1:markerEnd])
+        count += (x * y) + partOne(data[markerEnd + x + 1:])
+    }
+
+    return
+}
+
+func partTwo(data string) (count int) {
+    markerStart := strings.Index(data, "(")
+    markerEnd := strings.Index(data, ")")
+    if (markerStart == -1) || (markerEnd == -1) {
+        count += len(data)
+    } else {
+        if markerStart != 0 {
+            count += markerStart
+        }
+
+        x, y := parseMarker(data[markerStart + 1:markerEnd])
+        count += (partTwo(data[markerEnd+1:markerEnd+x+1]) * y) + (partTwo(data[markerEnd + x + 1:]))
+    }
+
+    return
+}
+
+func parseMarker(marker string) (x, y int) {
+    splitMarker := strings.Split(marker, "x")
+    x,_ = strconv.Atoi(splitMarker[0])
+    y,_ = strconv.Atoi(splitMarker[1])
+
+    return
+}
